b3da89113429faaf0c9b473e6bffe73d713a72b6
Louis-Philippe Gauthier <lpgauth@gmail.com>
2026-03-19 15:48:10 -0400

    Add checksums for v0.1.2

--- checksum-Elixir.Torque.Native.exs
+++ checksum-Elixir.Torque.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libtorque_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:225c2d6347a545de422b20924feb5ab8236a1ecd57cdb8a743246a55d1a87b30",
-  "libtorque_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:6283decf9c8b5e5c6f43a7d30c06d016926a66182555c71b1bfb115e271d19ac",
-  "libtorque_nif-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:a451fc9944e83368dd7944d024f7cf0ac68e35f591bdb790dce0b21a6b9b9562",
-  "libtorque_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d22f1c39aaba36e182eb67aa57b13d475b5cec9bab269a7a9e069b627aa6cc16",
+  "libtorque_nif-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:bc6c8ad6ebd7aad640a2c739fa8d5935b69f799c252a0508a111bf9a1188af47",
+  "libtorque_nif-v0.1.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b6e9ab1e86a41897cca51f15bfe205fd416dca8c852317a0ab431ecabdcc7ae1",
+  "libtorque_nif-v0.1.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:00f96b569946504d83f740e6c0d39a411235897a5476222c203dec8f9d72cc44",
+  "libtorque_nif-v0.1.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:eba0114a7db6a648882264d92546d84258e633e029971ff98d05bd270df439f6",
 }
