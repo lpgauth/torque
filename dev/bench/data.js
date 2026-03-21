960ac41329c6b56662512cdd0bc7578a42329af0
github-action-benchmark <github@users.noreply.github.com>
2026-03-21 21:43:55 +0000

    add Torque Benchmarks (customBiggerIsBetter) benchmark result for 17ca9a0b0a616e94b1a06a17f24989387a33d94e

---
+++ dev/bench/data.js
@@ -0,0 +1,222 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774129435421,
+  "repoUrl": "https://github.com/lpgauth/torque",
+  "entries": {
+    "Torque Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "lpgauth@gmail.com",
+            "name": "Louis-Philippe Gauthier",
+            "username": "lpgauth"
+          },
+          "committer": {
+            "email": "lpgauth@gmail.com",
+            "name": "Louis-Philippe Gauthier",
+            "username": "lpgauth"
+          },
+          "distinct": true,
+          "id": "17ca9a0b0a616e94b1a06a17f24989387a33d94e",
+          "message": "Fix simdjsone compilation",
+          "timestamp": "2026-03-21T17:37:24-04:00",
+          "tree_id": "b8f016ad50b9ae85390634806ab917b0aef19c5b",
+          "url": "https://github.com/lpgauth/torque/commit/17ca9a0b0a616e94b1a06a17f24989387a33d94e"
+        },
+        "date": 1774129435166,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "torque decode",
+            "value": 164519.8017098653,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone decode",
+            "value": 133485.2704881282,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy decode",
+            "value": 73404.51713932648,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json decode",
+            "value": 71443.79218081506,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason decode",
+            "value": 66277.47152125664,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque parse+get_many_nil",
+            "value": 127605.42140350907,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque parse+get_many",
+            "value": 127317.65230806464,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque parse+get",
+            "value": 101513.40473227116,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone parse+get",
+            "value": 76877.83330291425,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: proplist => binary",
+            "value": 791694.3679536396,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: proplist => iodata",
+            "value": 772076.907813022,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: map => iodata",
+            "value": 662285.6186005804,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: map => binary",
+            "value": 643232.7275494437,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json: map => iodata",
+            "value": 491590.495837705,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy: proplist => iodata",
+            "value": 406318.4399478911,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone: proplist => iodata",
+            "value": 392677.64369348926,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy: map => iodata",
+            "value": 339154.9450356695,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json: map => binary",
+            "value": 317630.77689053805,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason: map => iodata",
+            "value": 312063.4983536024,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone: map => iodata",
+            "value": 309128.3474508329,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason: map => binary",
+            "value": 227919.34538535416,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone decode",
+            "value": 257.47004348947513,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque decode",
+            "value": 241.6273459048657,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json decode",
+            "value": 111.83999780710522,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason decode",
+            "value": 85.95097918266823,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy decode",
+            "value": 55.89346199748514,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: proplist => binary",
+            "value": 572.0804259334856,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: proplist => iodata",
+            "value": 564.7258193728827,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: map => iodata",
+            "value": 477.8708138822127,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "torque: map => binary",
+            "value": 475.3511455989929,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy: proplist => iodata",
+            "value": 294.5378285645238,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json: map => iodata",
+            "value": 259.82768671864136,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jiffy: map => iodata",
+            "value": 247.94241862114083,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone: proplist => iodata",
+            "value": 242.2895990329278,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "simdjsone: map => iodata",
+            "value": 205.1336155209063,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason: map => iodata",
+            "value": 167.0860974741863,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "otp json: map => binary",
+            "value": 163.13305094630368,
+            "unit": "iterations/s"
+          },
+          {
+            "name": "jason: map => binary",
+            "value": 113.30380135628383,
+            "unit": "iterations/s"
+          }
+        ]
+      }
+    ]
+  }
+}
