aec4d0b7f48e7dc1d038fef53180476c1a421243
Louis-Philippe Gauthier <lpgauth@gmail.com>
2026-09-14 15:17:59 -0400

    Bump version to 0.4.1

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Add to your `mix.exs`:
 ```elixir
 def deps do
   [
-    {:torque, "~> 0.4.0"}
+    {:torque, "~> 0.4.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Torque.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.4.1"
   @source_url "https://github.com/lpgauth/torque"
 
   def project do
